b835c2ed21247ca49b3ec093e0ce014684ffebc0
Marcello Crescimbeni <giosuemc@gmail.com>
2026-02-18 17:52:17 -0500

    monitoreo de red | final push

---
+++ src/main/java/com/marcello/monitoreodered/MonitoreoDeRedApplication.java
@@ -0,0 +1,13 @@
+package com.marcello.monitoreodered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoDeRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoreoDeRedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcello/monitoreodered/MonitoreoDeRedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcello.monitoreodered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoDeRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
